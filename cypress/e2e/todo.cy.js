056939e55ca510762cb0a2a706a738da164ffc30
agustin rega <105014579+rega21@users.noreply.github.com>
2022-11-07 17:00:31 -0300

    Update todo.cy.js

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,10 +1,6 @@
 /// <reference types="cypress" />
 
 
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
 
 describe('example to-do app', () => {
     beforeEach(() => {
